96b71ecf3ac8b318cdb46f80521d654f64d36117
ayash01 <2231@tkmce.ac.in>
2022-12-07 15:12:28 +0530

    new commit

--- co3/1-built-in-packages.py
+++ co3/1-built-in-packages.py
@@ -1,7 +1,18 @@
 import os
 import math
 
-os.mkdir("C:\NewFolder")
-print("Folder C:\NewFolder created.")
 
-print("Square root of 64 is", math.sqrt(64))
+if (os.path.exists('C:/NewFolder') == False):
+    os.mkdir("C:/NewFolder")
+    print("Folder C:/NewFolder created.")
+else:
+    print("NewFolder already exists.")
+
+if (os.path.isfile('C:/NewFolder/newfile.txt') == True):
+    print("NewFile.txt already exists") 
+else:
+    f = open("C:/NewFolder/newfile.txt", "x")
+    
+    
+c = math.sqrt(64)
+f.write(c)
