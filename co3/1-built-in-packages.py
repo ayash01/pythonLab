1a4496651902744cb936cf9c3b27cf53b823a925
ayash01 <2231@tkmce.ac.in>
2022-12-12 09:31:18 +0530

    fix built-in package bug

--- co3/1-built-in-packages.py
+++ co3/1-built-in-packages.py
@@ -8,11 +8,13 @@ if (os.path.exists('C:/NewFolder') == False):
 else:
     print("NewFolder already exists.")
 
-if (os.path.isfile('C:/NewFolder/newfile.txt') == True):
+if (os.path.exists('C:/NewFolder/newfile.txt')):
     print("NewFile.txt already exists") 
+    f = open("C:/NewFolder/newfile.txt", "w")
 else:
     f = open("C:/NewFolder/newfile.txt", "x")
     
     
 c = math.sqrt(64)
-f.write(c)
+f.write(str(c))
+f.close()
