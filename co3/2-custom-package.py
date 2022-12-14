c3c5ccc9bb72733f92516d90c39d829df58e414f
ayash01 <2231@tkmce.ac.in>
2022-12-14 14:49:33 +0530

    fix co3

--- co3/2-custom-package.py
+++ co3/2-custom-package.py
@@ -1,7 +1,9 @@
 from graphics.graphics_3D import cuboid, sphere
 from graphics import circle, rectangle
 
-print("Area of circle:", circle.area(3, 4))
-print("Circumference of sphere:", sphere.cfr(3, 4))
+
+
+print("Area of circle:", circle.area(3))
+print("Circumference of sphere:", sphere.cfr(4))
 print("Area of rectangle:", rectangle.pm(5, 4))
 print("Perimeter of cuboid: ", circle.area(3, 4, 6))
