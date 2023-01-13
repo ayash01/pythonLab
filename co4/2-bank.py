2158c7abf1e281756b83e849f8331b38df343c1d
ayash01 <2231@tkmce.ac.in>
2023-01-13 10:40:21 +0530

    fix bank logic

--- co4/2-bank.py
+++ co4/2-bank.py
@@ -9,19 +9,13 @@ class bankAccount:
 
     def deposit(self, amt):
         self.amt = amt
-        if (self.amt <= 0):
-            return "Invalid amount"
-        else:
-            self.bal += self.amt
-            return self.bal
+        self.bal += self.amt
+        return self.bal
 
     def withdraw(self, amt):
         self.amt = amt
-        if (self.bal <= self.amt or self.amt <= 0):
-            return "Error"
-        else:
-            self.bal -= self.amt
-            return self.bal
+        self.bal -= self.amt
+        return self.bal
 
 os.system('cls||clear')
 
@@ -35,12 +29,20 @@ while True:
     ch = int(input("\nWhat do you want to do?: \n1. Deposit\n2. Withdraw\n3. Check Balance\n4. View account details\n\n0. Exit\n\n-> "))
     if (ch == 1):
         amt = int(input("\nEnter amount to deposit: "))
-        acc1.deposit(amt)
-        print("\nAmount ₹", amt, "Deposited")
+        if (amt <= 0):
+            print("\nInvalid amount")
+        else:
+            acc1.deposit(amt)
+            print("\n₹", amt, "Deposited")
     elif (ch == 2):
         amt = int(input("\nEnter amount to withdraw:"))
-        acc1.withdraw(amt)
-        print("\nAmount ₹", amt, "Withdrawn.")
+        if (amt > acc1.bal):
+            print("\nInsufficient Balance")
+        elif (amt <= 0):
+            print("\nInvalid amount")
+        else:
+            acc1.withdraw(amt)
+            print("\n₹", amt, "Withdrawn.")
     elif (ch == 3):
         print("\nCurrent balance:", acc1.bal)
     elif (ch == 4):
