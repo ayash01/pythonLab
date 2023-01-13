c0c1cb6abd276ecf5f8820f52aede8f574d3db1f
ayash01 <2231@tkmce.ac.in>
2023-01-13 11:07:14 +0530

    fix bank

--- co4/2-bank.py
+++ co4/2-bank.py
@@ -1,6 +1,7 @@
 import os
 import math
 
+
 class bankAccount:
     def __init__(self, accNo, name, accType, bal):
         self.accNo = accNo
@@ -18,6 +19,7 @@ class bankAccount:
         self.bal -= self.amt
         return self.bal
 
+
 os.system('cls||clear')
 
 accNo = int(input("Enter account number:"))
@@ -25,32 +27,35 @@ name = input("Enter account holder name:")
 accType = input("Enter account type:")
 bal = int(input("Enter initial amount:"))
 
-acc1 = bankAccount(accNo, name, accType, bal)
-
-while True:
-    ch = int(input("\nWhat do you want to do?: \n1. Deposit\n2. Withdraw\n3. Check Balance\n4. View account details\n\n0. Exit\n\n-> "))
-    if ch == 1:
-        amt = int(input("\nEnter amount to deposit: "))
-        if (amt <= 0):
-            print("\nInvalid amount")
+if bal >= 0:
+    acc1 = bankAccount(accNo, name, accType, bal)
+    while True:
+        ch = int(input(
+            "\nWhat do you want to do?: \n1. Deposit\n2. Withdraw\n3. Check Balance\n4. View account details\n\n0. Exit\n\n-> "))
+        if ch == 1:
+            amt = int(input("\nEnter amount to deposit: "))
+            if (amt <= 0):
+                print("\nInvalid amount")
+            else:
+                acc1.deposit(amt)
+                print("\n₹", amt, "Deposited")
+        elif ch == 2:
+            amt = int(input("\nEnter amount to withdraw:"))
+            if amt > acc1.bal:
+                print("\nInsufficient Balance")
+            elif amt <= 0:
+                print("\nInvalid amount")
+            else:
+                acc1.withdraw(amt)
+                print("\n₹", amt, "Withdrawn.")
+        elif ch == 3:
+            print("\nCurrent balance:", acc1.bal)
+        elif ch == 4:
+            print("\nAccount Number: ", acc1.accNo, "\n"+"Account Holder: ", acc1.name,"\n"+"Account Type: ", acc1.accType, "\n"+"Current Balance: ", acc1.bal)
+        elif ch == 0:
+            os.system('cls||clear')
+            break
         else:
-            acc1.deposit(amt)
-            print("\n₹", amt, "Deposited")
-    elif ch == 2:
-        amt = int(input("\nEnter amount to withdraw:"))
-        if amt > acc1.bal:
-            print("\nInsufficient Balance")
-        elif amt <= 0:
-            print("\nInvalid amount")
-        else:
-            acc1.withdraw(amt)
-            print("\n₹", amt, "Withdrawn.")
-    elif ch == 3:
-        print("\nCurrent balance:", acc1.bal)
-    elif ch == 4:
-        print("\nAccount Number: ", acc1.accNo, "\n"+"Account Holder: ", acc1.name, "\n"+"Account Type: ", acc1.accType, "\n"+"Current Balance: ", acc1.bal)
-    elif ch == 0:
-        os.system('cls||clear')
-        break
-    else:
-        print("\nInvalid input. Please try again.")
+            print("\nInvalid input. Please try again.")
+else:
+    print("\nInvalid amount\n")
